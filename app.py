c7ad390be751d015c4364ceb4206803c4fe4cc3a
Your Name <you@example.com>
2022-11-06 22:28:32 -0800

    add, edit, delete, posts

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -84,7 +84,8 @@ def update_user(user_id):
   db.session.add(user)
   db.session.commit()
 
-  return redirect("/users")
+  # why can't i redirect to /users/user_id correctly?
+  return redirect(f"/users/{user_id}")
 
 
 @app.route('/users/<int:user_id>/delete', methods=['POST'])
@@ -97,3 +98,67 @@ def delete_user(user_id):
   db.session.commit()
 
   return redirect('/users')
+
+
+@app.route('/users/<int:user_id>/posts/new')
+def new_post_form(user_id):
+  """Show New Post Form"""
+
+  user = User.query.get_or_404(user_id)
+
+  return render_template('new_post.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def new_post(user_id):
+  """ New post submission """
+
+  user = User.query.get_or_404(user_id)
+
+  title = request.form["title"]
+  content = request.form["content"]
+  
+
+  new_post = Post(title=title, content=content, user=user)
+
+  db.session.add(new_post)
+  db.session.commit()
+
+  return redirect(f"/users/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+  post = Post.query.get(post_id)
+
+  return render_template('post_details.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post_form(post_id):
+
+  post = Post.query.get(post_id)
+
+  return render_template('edit_post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def edit_post_update(post_id):
+
+  post = Post.query.get_or_404(post_id)
+  post.title = request.form['title']
+  post.content = request.form['content']
+  db.session.add(post)
+  db.session.commit()
+
+  return redirect(f'/users/{post.user_id}')
+
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+
+  post = Post.query.get(post_id)
+  db.session.delete(post)
+  db.session.commit()
+  
+  return redirect(f"/users/{post.user_id}")
--- models.py
+++ models.py
@@ -15,17 +15,33 @@ class User(db.Model):
 
   __tablename__= 'users'
 
-
   def __repr__(self):
     u = self
     return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}>" 
-
 
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   first_name = db.Column(db.Text, nullable=False)
   last_name = db.Column(db.Text, nullable=False)
   image_url = db.Column(db.Text, nullable=False, default=default_image_url)
 
+  posts = db.relationship("Post", backref="user")
+
   def get_full_name(self):
     """Gets first and last name"""
     return f'{self.first_name} {self.last_name}'
+
+
+class Post(db.Model):
+  """ Post made by user """
+
+  __tablename__='posts'
+
+  def __repr__(self):
+    p = self
+    return f"<Post Id={p.id} title={p.title} content={p.content} created_at={p.created_at}>" 
+
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.Text, nullable=False)
+  content = db.Column(db.Text, nullable=False)
+  created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=db.func.now())
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
